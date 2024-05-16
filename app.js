006ccaa202e0844640a8b70b269f2fd0413cfd13
dnlslcd <danielsalcedoq@gmail.com>
2024-05-16 19:07:21 +0200

    acabado

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 // ¡Buena suerte!
+
+const dice = document.querySelector("img")
+const pieceOfAdvice = document.querySelector(".card__quote");
+const adviceID = document.querySelector("#advice-id");
+
+    // 1. Al hacer clic en el dado, se debe hacer una petición GET mediante la funcion fetch a la URL adecuada
+dice.addEventListener('click', async function(){
+    // 2. Obtenemos un consejo nuevo
+    const response = await fetch("https://api.adviceslip.com/advice");
+    console.log("objeto response: ", response);
+    const data = await response.json();
+    console.log("objeto data: ", data);
+    // 3. Tenemos que actualizar el #ID del consejo y el texto del consejo, en el nodo DOM adecuado.
+    pieceOfAdvice.textContent = data.slip.advice; 
+    adviceID.textContent = data.slip.id;
+});
+
+    
